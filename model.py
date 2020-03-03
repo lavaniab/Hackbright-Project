774ba5035f826d634f9edb8480f2cda553539f2e
vagrant <vagrant@vagrant.vm>
2020-03-03 22:35:39 +0000

    added assc. table

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 """Models and datavase functions for Travel Journal"""
 
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy, Model  
 
 #This is the connection to PostgreSQL database from library
 #Find session object within where we do most of our interactions (like committing)
@@ -21,14 +21,14 @@ class User(db.Model):
 	def __repr__(self):
 
 		return f"<User fname={self.fname} email={self.email}>"
-		
+
 
 class Entry(db.Model):
 
 	__tablename__ = "entries"
 
 	entry_id = db.Column(db.String(100), autoincrement=True, primary_key=True)
-	user_id = db.Column(db.Integer, foreign_key=True(User.user_id))
+	user_id = db.Column(db.Integer, foreign_key("user.user_id"))
 	user_entry = db.Column(db.String(1500), nullable=False)
 	user_picture = db.Column(db.URL(200), nullable=True) ## ref url for third party image hosting
 	
@@ -41,13 +41,15 @@ class Trip(db.Model):
 	__tablename__ = "trips"
 
 	trip_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-	entry_id = db.Column(db.Integer, foreign_key=True(Entry.entry_id))
-	location_id = db.Column(db.String(500),foreign_key=True(Location.location_id), nullable=True)
+	entry_id = db.Column(db.Integer, foreign_key("entries.entry_id"))
+	location_id = db.Column(db.String(500),foreign_key("locations.location_id"), nullable=True)
 	user_description = db.Column(db.String(160), nullable=True)
 
+	trip_entries = db.relationship("Entry", backref="trips")
 
 class Location(db.Model): #for now this will just be a nice little box with saved info that appears
-						  # visions of a 'clickable' button that would take user to another page with the spot pointed out in a map
+	"""Locations of the trip."""  # visions of a 'clickable' button that would take user to another page with the spot pointed out in a map
+
 	__tablename__ = "locations"
 
 	location_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
@@ -56,8 +58,20 @@ class Location(db.Model): #for now this will just be a nice little box with save
 	state = db.Column(db.String(100), nullable=True)
 	country = db.Column(db.String(100), nullable=True)
 
+	locations = db.relationship("Trip", backref="locations") 
 
-db.create_all()
+
+class Association_Table(db.Model):
+	"""Association between trips and locations."""
+
+	__tablename__ = "locationsTrips"
+
+	locationsTrips_id = db.Column(db.Integer, autoincrement=True)
+	location_id = db.Column(db.Integer, foreign_key("locations.location_id"))
+	trip_id = db.Column(db.Integer, foreign_key("trips.trip_id"))
+
+
+#db.create_all()
 
 ###################################################################
 
