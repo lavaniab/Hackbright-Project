efc6db3186a6a545f66a3c33ca6a399912b444ed
vagrant <vagrant@vagrant.vm>
2020-03-04 21:10:11 +0000

    3 of 5 tables up and running

--- model.py
+++ model.py
@@ -1,30 +1,22 @@
 """Models and database functions for Travel Journal"""
 from flask import Flask 
-from flask_sqlalchemy import SQLAlchemy, Model  
+from flask_sqlalchemy import SQLAlchemy, Model
+#import correlation
+#from collections import defaultdict
 
 #This is the connection to PostgreSQL database from library
 #Find session object within where we do most of our interactions (like committing)
-# app = Flask(__name__) Do I need this here if in server.py????
 
-DB_URI = "postgresql:///Travel_journaldb" # Unsure of what this does, also...table names or??
+SQLALCHEMY_DATABASE_URI = "postgresql:///Travel_journaldb" # Unsure of what this does, also...table names or??
 db = SQLAlchemy()
 
-# def connect_to_db(app, Travel_journaldb): #(app, db_name) #should I move to model.py??
-#     """Connect to database."""
-
-#     app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql:///{Travel_journaldb}"
-#     app.config["SQLALCHEMY_ECHO"] = True
-#     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-#     db.app = app
-#     db.init_app(app)
 
 class User(db.Model):
 	"""User of travel journal site"""
 
 	__tablename__ = "users"
 
-	user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+	user_id = db.Column(db.Integer, primary_key=True)
 	fname = db.Column(db.String(25), nullable=False)
 	lname = db.Column(db.String(25), nullable=False)
 	email = db.Column(db.String(64), nullable=False, unique=True)
@@ -40,7 +32,7 @@ class Entry(db.Model):
 
 	__tablename__ = "entries"
 
-	entry_id = db.Column(db.String(100), autoincrement=True, primary_key=True)
+	entry_id = db.Column(db.Integer, primary_key=True)
 	user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
 	user_entry = db.Column(db.String(1500), nullable=False)
 	user_picture = db.Column(db.String(200), nullable=True) ## ref url for third party image hosting
@@ -58,9 +50,9 @@ class Trip(db.Model):
 
 	__tablename__ = "trips"
 
-	trip_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+	trip_id = db.Column(db.Integer, primary_key=True)
 	entry_id = db.Column(db.Integer, db.ForeignKey("entries.entry_id"))
-	location_id = db.Column(db.String(500), db.ForeignKey("locations.location_id"), nullable=True)
+	location_id = db.Column(db.Integer, db.ForeignKey("locations.location_id"), nullable=True)
 	user_description = db.Column(db.String(160), nullable=True)
 
 	trip_entries = db.relationship("Entry", backref="trips")
@@ -70,7 +62,7 @@ class Location(db.Model): #for now this will just be a nice little box with save
 
 	__tablename__ = "locations"
 
-	location_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+	location_id = db.Column(db.Integer, primary_key=True)
 	address = db.Column(db.String(100), nullable=True)
 	city = db.Column(db.String(100), nullable=True)
 	state = db.Column(db.String(100), nullable=True)
@@ -84,7 +76,7 @@ class Association_Table(db.Model):
 
 	__tablename__ = "locationsTrips"
 
-	locationsTrips_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+	locationsTrips_id = db.Column(db.Integer, primary_key=True)
 	location_id = db.Column(db.Integer, db.ForeignKey("locations.location_id"))
 	trip_id = db.Column(db.Integer, db.ForeignKey("trips.trip_id"))
 
@@ -97,7 +89,7 @@ def connect_to_db(app):
 	"""Connect the database to our Flask app."""
 
 	# Configure to use our PostgreSQL database
-	app.config['SQLALCHEMY_	DATABASE_URI'] = 'postgresql:///travel_journaldb' 
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///Travel_journaldb' 
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 	app.config['SQLALCHEMY_ECHO'] = True
 	db.app = app
@@ -105,10 +97,12 @@ def connect_to_db(app):
 
 if __name__ == "__main__":
 
-	# from flask import Flask
 	from server import app
-	# app = Flask(__name__)
 
 	connect_to_db(app)
+	#drop_all()
 	db.create_all()
+	#db.session.commit()
+	#app.run()
+
 	print("Connect to DB.")
